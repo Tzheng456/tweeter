8065401b9b77b24f888bb0955695dda2a74e37ea
Tzheng456 <Tzheng456@gmail.com>
2021-08-11 04:00:28 +0000

    remove console logs in tweet-animations.js; add box-shadow for tweet css

--- public/scripts/tweet-animations.js
+++ public/scripts/tweet-animations.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
+  $(".tweet-container").on("mouseenter", function(event) {
+    $(this).css("box-shadow", "7px 7px #d4d4d4");
+  });
+  $(".tweet-container").on("mouseleave", function(event) {
+    $(this).css("box-shadow", "none");
+  });
   $(".response-buttons").on("mouseenter", function(event) {
     $(this).css("color", "#f5b800");
   });
