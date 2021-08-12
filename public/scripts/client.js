c3f324555a52b6d5f21537c8c4d355ca9b73c974
Tzheng456 <Tzheng456@gmail.com>
2021-08-12 02:44:34 +0000

    move mouse event handlers for tweet animations into client.js async call

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -8,6 +8,18 @@ $(() => {
     for (let tweet of tweets) {
       $("#tweets-container").append(createTweetElement(tweet));
     }
+    $(".tweet-container").on("mouseenter", function(event) {
+      $(this).css("box-shadow", "7px 7px #d4d4d4");
+    });
+    $(".tweet-container").on("mouseleave", function(event) {
+      $(this).css("box-shadow", "none");
+    });
+    $(".response-buttons").on("mouseenter", function(event) {
+      $(this).css("color", "#f5b800");
+    });
+    $(".response-buttons").on("mouseleave", function(event) {
+      $(this).css("color", "#2968cb");
+    });
   };
 
   const createTweetElement = function(tweet) {
@@ -30,6 +42,7 @@ $(() => {
       </div>`;
 
     const $tweet = $("<article>").addClass("tweet-container");
+    $tweet.empty();
 
     const tweetElement = $tweet.append(tweetHTML);
 
